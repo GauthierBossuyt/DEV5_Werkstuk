3ee19aafc09e9c8a297f1aa63484c2f068849680
Gauthier Bossuyt <55983896+GauthierBossuyt@users.noreply.github.com>
2021-11-06 13:53:28 +0100

    feat(server): setup server with express.js

--- API/source/index.js
+++ API/source/index.js
@@ -1,2 +1,24 @@
-console.log(`Hello ${process.env.POSTGRES_PASSWORD}`);
+//PACKAGES
+const express = require("express");
+const bodyParser = require("body-parser");
 
+//GLOBAL VARIABLES
+const PORT = process.env.PORT;
+const SERVER = express();
+
+
+//FUNCTIONS
+SERVER.use(bodyParser.json());
+SERVER.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+SERVER.get("/", function (req, res) {
+  res.send("Hello");
+});
+
+SERVER.listen(PORT, () => {
+  console.log(`SERVER is listening at port ${PORT}`);
+});
